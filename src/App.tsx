4da4f260a7794c40a4c9ab1c3d72fe12f6dd557a
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-08-15 15:55:37 -0300

    Configurando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="danger" />
       <Button variant="success" />
-    </>
+    </ThemeProvider>
   );
 };
 
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,8 +16,15 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-  ${({ variant }) =>
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+  background: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
+
+  /* ${({ variant }) =>
     css`
       background-color: ${buttonVariants[variant]};
-    `}
+    `} */
 `;
