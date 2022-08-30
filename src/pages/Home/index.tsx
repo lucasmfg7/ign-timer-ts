7282b997f3ebf0eab5031e94574c0aedcf4bf013
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-08-30 10:27:51 -0300

    Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,14 +17,23 @@ export const Home = () => {
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
+            list="task-suggestions"
             type="text"
             placeholder="Dê um nome para o seu projeto"
           />
+
+          <datalist id="task-suggestions">
+            <option value="" />
+          </datalist>
+
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             id="minutesAmount"
             type="number"
             placeholder="00"
+            step={5}
+            min={0}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
