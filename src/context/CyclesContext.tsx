9fb069fd58eb104e6f51d539e26eaa5162ff489a
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-10-27 17:04:20 -0300

    Reset do formulário

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -72,8 +72,6 @@ export const CyclesContextProvider = ({
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
